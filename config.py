bcab1399667f5faee10100060fb78866490a15f5
bilin.wang <wangbilin@orientsoft.cn>
2019-05-31 12:03:28 +0800

    初始化租户

--- config.py
+++ config.py
@@ -1,33 +1,10 @@
-SESSION_TYPE = 'mongodb'  # session的mongo地址
-SESSION_MONGODB = 'mongodb://192.168.0.48:38213'
-# session的db名称
-SESSION_MONGODB_DB = 'MOOP_SERVICE'
-# session的collection名称
-SESSION_MONGODB_COLLECT = 'sessions'
-# session的前缀
-SESSION_KEY_PREFIX = 'session'
-# session超时时间
-# 加密key
-SECRET_KEY = 'abcdefg'
+# -*- coding:utf-8 -*-
 # 程序的mongo地址
 MONGODB_URL = 'mongodb://192.168.0.31:32017/MOOP'
-# 日志格式
-LOG_FORMAT = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s:%(funcName)s - [%(levelname)s] %(message)s'
 # 租户service的地址
-MOOP_TENANT_SERVICE_URL = 'http://192.168.0.48:7778/service/v1'
+# MOOP_TENANT_SERVICE_URL = 'http://192.168.0.31:31778/service/v1/tenants'
+MOOP_TENANT_SERVICE_URL = 'http://192.168.0.129:7778/service/v1/tenants'
 # project service的地址
 MOOP_PROJECT_SERVICE_URL = 'http://192.168.0.48:7779/service/v1'
 # volume service的地址
-MOOP_VOLUME_SERVICE_URL = 'http://192.168.0.48:5010/service/v1/volumes'
-# pod service的地址
-MOOP_POD_SERVICE_URL = 'http://192.168.0.48:5020/service/v1'
-# launcher service的地址
-MOOP_LAUNCHER_SERVICE_URL = 'http://192.168.0.31:30264/services/launcher'
-# sms service的地址
-SMS_SERVICE_URL = 'http://192.168.0.31:31785/api/v1'
-# es service的地址
-AUDIT_SERVICE_URL = 'http://192.168.0.31:31786'
-# report service的地址
-REPORT_SERVICE_URL = 'http://192.168.0.31:31787/api/v1'
-# matplot service的地址
-MATPLOT_SERVICE_URL = 'http://192.168.0.31:31788/api/v1'
+MOOP_VOLUME_SERVICE_URL = 'http://192.168.0.31:31710/service/v1/volumes'
---
+++ tenant.py
@@ -0,0 +1,211 @@
+# -*- coding:utf-8 -*-
+import requests
+import config
+import time
+
+
+class Tenant:
+    def __init__(self, obj):
+        '''
+        :param obj:  {'name':'租户名称','namespace':'租户名空间','remark':'租户的简介描述'}
+        '''
+        self.name = obj['name']
+        self.namespace = obj['namespace']
+        self.remark = obj['remark']
+
+    def createTenant(self):
+        tenant_json = {
+            "activated": True,
+            "logo": None,
+            "name": self.name,
+            "namespace": self.namespace,
+            "remark": self.remark,
+            "resources": {
+                "templates": {
+                    "pod": {
+                        "apiVersion": "v1",
+                        "kind": "Pod",
+                        "metadata": {
+                            "name": "job-{}-{}"
+                        },
+                        "spec": {
+                            "containers": [
+                                {
+                                    "args": [
+                                        "/bin/sh",
+                                        "-c",
+                                        "{}"
+                                    ],
+                                    "image": "registry.orientsoft.cn/gitbox/alpine-git:lastest",
+                                    "imagePullPolicy": "IfNotPresent",
+                                    "name": "copy",
+                                    "volumeMounts": []
+                                }
+                            ],
+                            "restartPolicy": "Never",
+                            "ttlSecondsAfterFinished": 0,
+                            "volumes": []
+                        }
+                    },
+                    "pv": {
+                        "apiVersion": "v1",
+                        "kind": "PersistentVolume",
+                        "metadata": {
+                            "name": "pv-{}-{}-{}",
+                            "namespace": "{}",
+                            "labels": {
+                                "pv": "pv-{}-{}-{}"
+                            }
+                        },
+                        "spec": {
+                            "accessModes": ["ReadWriteMany"],
+                            "capacity": {
+                                "storage": "100Mi"
+                            },
+                            "nfs": {
+                                "server": "{}",
+                                "path": "{}{}"
+                            }
+                        }
+                    },
+                    "match_pvc": {
+                        "apiVersion": "v1",
+                        "kind": "PersistentVolumeClaim",
+                        "metadata": {
+                            "name": "pvc-{}-{}-{}",
+                            "namespace": "{}"
+                        },
+                        "spec": {
+                            "accessModes": [
+                                "ReadWriteMany"
+                            ],
+                            "resources": {
+                                "requests": {
+                                    "storage": "100Mi"
+                                }
+                            },
+                            "selector": {
+                                "matchLabels": {
+                                    "pv": "pv-{}-{}-{}"
+                                }
+                            },
+                            "storageClassName": ""
+                        }
+                    },
+                    "pvc": {
+                        "apiVersion": "v1",
+                        "kind": "PersistentVolumeClaim",
+                        "metadata": {
+                            "name": "pvc-{}-{}-{}",
+                            "namespace": "{}"
+                        },
+                        "spec": {
+                            "accessModes": ["ReadWriteMany"],
+                            "storageClassName": "standard",
+                            "resources": {
+                                "requests": {
+                                    "storage": "100Mi"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        resp = requests.post(config.MOOP_TENANT_SERVICE_URL, json=tenant_json)
+        if resp.status_code == 200:
+            result = resp.json()
+            print('创建租户成功，租户编号为：', result['id'])
+            self.tenant = result['id']
+        else:
+            print('创建租户失败，原因：', resp.text)
+            raise
+
+    def deleteTenant(self):
+        try:
+            requests.delete(config.MOOP_TENANT_SERVICE_URL + '/' + self.tenant)
+        except:
+            pass
+
+    def deleteVoume(self):
+        try:
+            payload = {
+                'tenant': self.tenant,
+                'username': "super",
+                'tag': self.namespace
+            }
+            requests.delete(config.MOOP_VOLUME_SERVICE_URL + '/pvcs', params=payload)
+            requests.delete(config.MOOP_VOLUME_SERVICE_URL + '/pvs', params=payload)
+        except:
+            pass
+
+    def addPV(self):
+        pv_json = {
+            "tenant": self.tenant,
+            "username": "super",
+            "tag": self.namespace,
+            "path": self.namespace
+        }
+        payload = {
+            'tenant': self.tenant,
+            'username': "super",
+            'tag': self.namespace
+        }
+        # 创建pv
+        requests.post(config.MOOP_VOLUME_SERVICE_URL + '/pvs', json=pv_json)
+        # 查询pv状态
+        while True:
+            resp = requests.get(config.MOOP_VOLUME_SERVICE_URL + '/pvs', params=payload)
+            if resp.status_code == 200:
+                result = resp.json()
+                if result['status']['phase'] != 'Pending':
+                    print('PV创建成功')
+                    break
+                print('PV创建进度：', result['status']['phase'])
+                time.sleep(1)
+            else:
+                print('PV创建失败原因：', resp.text)
+                raise
+
+    def addPVC(self):
+        pvc_json = {
+            "tenant": self.tenant,
+            "username": "super",
+            "tag": self.namespace,
+            "match": True
+        }
+        payload = {
+            'tenant': self.tenant,
+            'username': "super",
+            'tag': self.namespace
+        }
+        # 创建pvc
+        requests.post(config.MOOP_VOLUME_SERVICE_URL + '/pvcs', json=pvc_json)
+        # 查询pvc状态
+        while True:
+            resp = requests.get(config.MOOP_VOLUME_SERVICE_URL + '/pvcs', params=payload)
+            if resp.status_code == 200:
+                result = resp.json()
+                if result['status']['phase'] != 'Pending':
+                    print('PVC创建成功')
+                    break
+                print('PVC创建进度：', result['status']['phase'])
+                time.sleep(1)
+            else:
+                print('PVC创建失败原因：', resp.text)
+                raise
+
+
+if __name__ == '__main__':
+    tenant = Tenant({'name': '测试租户', 'namespace': 'moop-test', 'remark': '测试租户的简介描述'})
+    try:
+        # 1.创建租户账号
+        tenant.createTenant()
+        # 2.增加pv
+        tenant.addPV()
+        # 3.增加PVC
+        tenant.addPVC()
+    except:
+        print('租户创建失败，回滚')
+        tenant.deleteTenant()
+        tenant.deleteVoume()
